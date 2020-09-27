4e539672b67c6c24615d5f8ec1f9d2fd1aba4488
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-09-28 03:42:40 +0545

    FlatList

--- App.js
+++ App.js
@@ -1,31 +1,38 @@
 import React from "react";
 import { useState } from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 
 export default function App() {
-  const [name, setName] = useState("Sparsh");
-  const [age, setAge] = useState("21");
-
-  const inputHandler = (e) => {
-    if (e.key === "Enter") {
-      setName(e.value);
-    }
-  };
+  const [name, setName] = useState([
+    { name: "Sparsh", id: 1 },
+    { name: "Shubham", id: 2 },
+    { name: "Shipra", id: 3 },
+    { name: "Shagun", id: 4 },
+    { name: "Sharma", id: 5 },
+    { name: "Sarthak", id: 6 },
+    { name: "Shrishti", id: 7 },
+    { name: "Shreya", id: 8 },
+    { name: "Shavi", id: 9 },
+    { name: "Shayla", id: 10 },
+  ]);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>My name is:</Text>
-      <View style={styles.inputButton}>
-        <TextInput
-          placeholder="e.g. John Doe"
-          style={styles.input}
-          onChangeText={(e) => setName(e)}
-        />
-        <Button title="Enter" />
-      </View>
-      <Text>
-        Name: {name}, Age: {age}
-      </Text>
+      <FlatList
+        keyExtractor={(item) => item.id}
+        data={name}
+        renderItem={({ item }) => <Text style={styles.text}>{item.name}</Text>}
+      />
+
+      {/* <ScrollView>
+        {name.map((item) => {
+          return (
+            <View key={item.key} style={styles.textContainer}>
+              <Text style={styles.text}>{item.name}</Text>
+            </View>
+          );
+        })}
+      </ScrollView> */}
     </View>
   );
 }
@@ -34,22 +41,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 40,
+    paddingHorizontal: 15,
+    // alignItems: "center",
+    // justifyContent: "center",
   },
   text: {
-    fontSize: 20,
-    color: "darkslateblue",
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#777",
-    padding: 8,
-    width: 200,
     margin: 10,
+    fontSize: 20,
+    padding: 20,
+    color: "#fff",
+    textAlign: "center",
+    backgroundColor: "darkslateblue",
   },
-  inputButton: {
-    display: "flex",
-    flexDirection: "row",
+  textContainer: {
+    overflow: "hidden",
+    borderRadius: 10,
   },
 });
