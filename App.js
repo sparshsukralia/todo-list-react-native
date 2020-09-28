097ca7669975ab21df8230e830b97d55d2d4e6f9
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-09-29 03:31:57 +0545

    to-do list

--- App.js
+++ App.js
@@ -7,73 +7,40 @@ import {
   FlatList,
   TouchableOpacity,
 } from "react-native";
+import Header from "./components/Header";
 
 export default function App() {
-  const [name, setName] = useState([
-    { name: "Sparsh", id: 1 },
-    { name: "Code", id: 2 },
-    { name: "Facebook", id: 3 },
-    { name: "NF", id: 4 },
-    { name: "Eminem", id: 5 },
-    { name: "MGK", id: 6 },
-    { name: "TuPac", id: 7 },
-    { name: "Larry Page", id: 8 },
-    { name: "Silicon Valley", id: 9 },
-    { name: "Dustin Moskovitz", id: 10 },
+  const [todos, setTodos] = useState([
+    { text: "Make a tutorial", key: "1" },
+    { text: "Bing Watch Sherlock", key: "2" },
+    { text: "Make an app", key: "3" },
   ]);
-
-  const pressHandler = (id) => {
-    console.log(id);
-    setName((prevName) => {
-      return prevName.filter((person) => person.id != id);
-    });
-  };
 
   return (
     <View style={styles.container}>
-      <FlatList
-        numColumns={2}
-        keyExtractor={(item) => item.id}
-        data={name}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-            <Text style={styles.text}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
-
-      {/* <ScrollView>
-        {name.map((item) => {
-          return (
-            <View key={item.key} style={styles.textContainer}>
-              <Text style={styles.text}>{item.name}</Text>
-            </View>
-          );
-        })}
-      </ScrollView> */}
+      {/* header */}
+      <Header />
+      <View style={styles.content}>
+        {/* to form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => <Text>{item.text}</Text>}
+          />
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#fff",
-    paddingTop: 40,
-    paddingHorizontal: 15,
-    // alignItems: "center",
-    // justifyContent: "center",
   },
-  text: {
-    margin: 10,
-    fontSize: 20,
-    padding: 20,
-    color: "#fff",
-    textAlign: "center",
-    backgroundColor: "darkslateblue",
+  content: {
+    padding: 40,
   },
-  textContainer: {
-    overflow: "hidden",
-    borderRadius: 10,
+  list: {
+    marginTop: 20,
   },
 });
---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Header() {
+  return (
+    <View>
+      <Text>To-Do List</Text>
+    </View>
+  );
+}
